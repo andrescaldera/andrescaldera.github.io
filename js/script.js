33fe5a956fc8890f4c0bc6d0b3383438e05b257e
DavidCortizo <cortizo7269@gmail.com>
2017-01-09 17:35:23 +0100

    figcaption
    inclui figcaption

--- js/script.js
+++ js/script.js
@@ -17,4 +17,39 @@ $(document).ready(function(){
     });
   });
 
+  // preview photo
+  $(".preview").hide();
+  $(".gallery-item").click(function(){
+    $(".preview").show();
+    $("body").css("overflow", "hidden");
+
+    $(this).children("img").clone().appendTo("#picture");
+
+    $(".left-nav").click(function(){
+      $("#picture").children().remove();
+      $(this).prev().children("img").clone().appendTo("#picture");
+    });
+    $(".right-nav").click(function(){
+      $("#picture").children().remove();
+      $(this).next().children("img").clone().appendTo("#picture");
+    });
+
+    var fCaption = $(this).find("figcaption").html();
+    console.log(fCaption);
+    $("#figCaption").html(fCaption);
+
+    $("#picture").children().css({"position": "absolute","display": "block","max-width": "100%","max-height": "100%","left": "0","right": "0","top": "0","bottom": "0","margin": "auto"});
+    $("#picture").children().click(function(){
+      $("#picture").children().css("transform","scale(1)");
+    });
+
+    $(".close-preview").click(function(){
+      $(".preview").hide();
+      $("body").css("overflow", "auto");
+      $("#picture").children().remove();
+    });
+
+  });// end preview photo
+
+
 });
