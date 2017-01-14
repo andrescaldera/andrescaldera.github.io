755bbbb8d2055b57daae03a26e4a5662058925d0
DavidCortizo <cortizo7269@gmail.com>
2017-01-14 16:51:11 +0100

    SAL
    same as last

--- js/script.js
+++ js/script.js
@@ -30,28 +30,4 @@ $(document).ready(function(){
   });
   // end parallax
 
-  // justifiedGallery
-  $("#galleryContainer").justifiedGallery({
-    rowHeight : 300,
-    lastRow : 'nojustify',
-    margins : 3,
-    rel: "gallery"
-  });
-  // end justifiedGallery
-  // swipebox
-  $('.swipebox').swipebox({
-    removeBarsOnMobile : true, // false will show top navigation bar on mobile devices
-    hideCloseButtonOnMobile : false, // true will hide the close button on mobile devices
-    //removeBarsOnMobile : true,  false will show bottom bar on mobile devices
-    hideBarsDelay : 20000000, // delay before hiding bars on desktop
-    videoMaxWidth : 1140,
-    loopAtEnd: false,
-    autoplayVideos: false,
-    useSVG: true
-  });
-  // end swipebox
-
-
-
-
 });
