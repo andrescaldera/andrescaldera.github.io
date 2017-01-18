64fd007eaee548861ae2834cd2b668c9ae7f8222
DavidCortizo <cortizo7269@gmail.com>
2017-01-18 22:50:12 +0100

    instafeed anadido
    instafeed 1rst

--- js/script.js
+++ js/script.js
@@ -18,16 +18,42 @@ $(document).ready(function(){
     });
   });
   // end mobile-nav
+  // carousel
+  $("#slideshow > .banner-img:gt(0)").hide();
+
+  setInterval(function(){
+    $("#slideshow > .banner-img:first")
+      .fadeOut(1000)
+      .next()
+      .fadeIn(1000)
+      .end()
+      .appendTo("#slideshow");
+  }, 8000);
+  // end carousel
 
   // parallax
   $(window).scroll(function(){
 
     var wScroll = $(this).scrollTop();
 
-    $(".banner-img img").css({
+    $(".banner-img").css({
       "transform" : "translate(0px, "+ wScroll/8 + "%)"
     });
   });
   // end parallax
+
+  // instafeed
+  var feed = new Instafeed({
+    clientId: 'de20c8b1dc394285aa23db84b32cc803',
+    accessToken: '2226cc8699fe4eab84b305f64109722f',
+    get: 'user',
+    userId: '3457657437',
+    template: '<a href="{{link}}"><img src="{{image}}"/></a>',
+    sortBy: 'most-recent',
+    limit: '6',
+    resolution: 'thumbnail'
+   });
+   feed.run();
+  // end instafeed
 
 });
