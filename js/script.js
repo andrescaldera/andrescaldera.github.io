d7976f5fd91d207d7a8da34b5d2e7cda929f5c75
DavidCortizo <cortizo7269@gmail.com>
2017-02-04 17:06:03 +0100

    index/photography
    improved banner static

--- js/script.js
+++ js/script.js
@@ -19,15 +19,21 @@ $(document).ready(function(){
   // end mobile-nav
 
   // carousel
-  $("#slideshow > .banner-img:gt(0)").hide();
-  setInterval(function(){
-    $("#slideshow > .banner-img:first")
+  var $window = $(window);
+  if ($window.innerWidth() > 746) {
+    $("#slideshow > .banner-img:gt(0)").hide();
+    setInterval(function(){
+      $("#slideshow > .banner-img:first")
       .fadeOut(1000)
       .next()
       .fadeIn(1000)
       .end()
       .appendTo("#slideshow");
-  }, 8000);
+    }, 8000);
+  }
+  else if ($window.innerWidth() <= 746) {
+    $("#slideshow > .banner-img:gt(0)").hide();
+  }
   // end carousel
 
   // parallax
@@ -43,5 +49,34 @@ $(document).ready(function(){
 
   });
   // end parallax
+
+  // gallery mobile
+  var $moreButton = $(".more-button");
+  var $indexGalleryItem = $(".index-gallery-item:nth-of-type(n+2)");
+  var $indexGalleryMobile = $(".index-gallery-mobile");
+  var $open = $(".open");
+  var $close = $(".close");
+
+  $close.hide();
+
+  $open.on("click", function(){
+    $indexGalleryMobile.css({"height":"100%"});
+    $indexGalleryItem.css({
+      "display":"flex",
+      "flex-flow":"column"
+    });
+    $open.hide();
+    $close.show();
+  });
+
+  $close.on("click", function(){
+    $indexGalleryMobile.css({"height":"85vw"});
+    $indexGalleryItem.css({
+      "display":"none",
+    });
+    $close.hide();
+    $open.show();
+  });
+  // end gallery mobile
 
 });
