8c8fd0615d9d077edbf12b9dcf81b233b9e1a1c9
DavidCortizo <cortizo7269@gmail.com>
2017-03-04 11:07:44 +0100

    gallery.js reworked

--- js/gallery.js
+++ js/gallery.js
@@ -10,17 +10,17 @@ $(document).ready(function(){
     $(".gallery a").click(function(event){
       event.preventDefault();});
     $('.swipebox').swipebox({
-      removeBarsOnMobile : true, // false will show top navigation bar on mobile devices
-      hideCloseButtonOnMobile : false, // true will hide the close button on mobile devices
-      //removeBarsOnMobile : true,  false will show bottom bar on mobile devices
-      hideBarsDelay : 20000000, // delay before hiding bars on desktop
+      removeBarsOnMobile : true,
+      hideCloseButtonOnMobile : false,
+      // removeBarsOnMobile : true,  false will show bottom bar on mobile devices
+      hideBarsDelay : 20000000,
       videoMaxWidth : 1140,
       loopAtEnd: false,
       autoplayVideos: false,
       useSVG: true
     });
   });
-  $("#galleryContainerDetailsSC").justifiedGallery({
+  $("#galleryContainer").justifiedGallery({
     rowHeight : 300,
     maxRowHeight : 300,
     lastRow : 'nojustify',
@@ -30,70 +30,10 @@ $(document).ready(function(){
     $(".gallery a").click(function(event){
       event.preventDefault();});
     $('.swipebox').swipebox({
-      removeBarsOnMobile : true, // false will show top navigation bar on mobile devices
-      hideCloseButtonOnMobile : false, // true will hide the close button on mobile devices
-      //removeBarsOnMobile : true,  false will show bottom bar on mobile devices
-      hideBarsDelay : 20000000, // delay before hiding bars on desktop
-      videoMaxWidth : 1140,
-      loopAtEnd: false,
-      autoplayVideos: false,
-      useSVG: true
-    });
-  });
-  $("#galleryContainerFireS").justifiedGallery({
-    rowHeight : 300,
-    maxRowHeight : 300,
-    lastRow : 'nojustify',
-    margins : 15,
-    rel: "gallery"
-  }).on("jg.complete", function(){
-    $(".gallery a").click(function(event){
-      event.preventDefault();});
-    $('.swipebox').swipebox({
-      removeBarsOnMobile : true, // false will show top navigation bar on mobile devices
-      hideCloseButtonOnMobile : false, // true will hide the close button on mobile devices
-      //removeBarsOnMobile : true,  false will show bottom bar on mobile devices
-      hideBarsDelay : 20000000, // delay before hiding bars on desktop
-      videoMaxWidth : 1140,
-      loopAtEnd: false,
-      autoplayVideos: false,
-      useSVG: true
-    });
-  });
-  $("#galleryContainerPhilarmonie").justifiedGallery({
-    rowHeight : 300,
-    maxRowHeight : 300,
-    lastRow : 'nojustify',
-    margins : 15,
-    rel: "gallery"
-  }).on("jg.complete", function(){
-    $(".gallery a").click(function(event){
-      event.preventDefault();});
-    $('.swipebox').swipebox({
-      removeBarsOnMobile : true, // false will show top navigation bar on mobile devices
-      hideCloseButtonOnMobile : false, // true will hide the close button on mobile devices
-      //removeBarsOnMobile : true,  false will show bottom bar on mobile devices
-      hideBarsDelay : 20000000, // delay before hiding bars on desktop
-      videoMaxWidth : 1140,
-      loopAtEnd: false,
-      autoplayVideos: false,
-      useSVG: true
-    });
-  });
-  $("#galleryContainerLaDefense").justifiedGallery({
-    rowHeight : 300,
-    maxRowHeight : 300,
-    lastRow : 'nojustify',
-    margins : 15,
-    rel: "gallery"
-  }).on("jg.complete", function(){
-    $(".gallery a").click(function(event){
-      event.preventDefault();});
-    $('.swipebox').swipebox({
-      removeBarsOnMobile : true, // false will show top navigation bar on mobile devices
-      hideCloseButtonOnMobile : false, // true will hide the close button on mobile devices
-      //removeBarsOnMobile : true,  false will show bottom bar on mobile devices
-      hideBarsDelay : 20000000, // delay before hiding bars on desktop
+      removeBarsOnMobile : true,
+      hideCloseButtonOnMobile : false,
+      // removeBarsOnMobile : true,  false will show bottom bar on mobile devices
+      hideBarsDelay : 20000000,
       videoMaxWidth : 1140,
       loopAtEnd: false,
       autoplayVideos: false,
@@ -114,27 +54,6 @@ $(document).ready(function(){
   //   useSVG: true
   // });
   // end swipebox
-
-  // $(".gallery").each(function (i, el){
-  //   $(el).justifiedGallery({
-  //     rel: "gallery-" + i,
-  //     rowHeight : 200,
-  //     // maxRowHeight : 300,
-  //     lastRow : 'nojustify',
-  //     margins : 15,
-  //   }).on("jg.complete", function(){
-  //     $(this).find("a").swipebox({
-  //       removeBarsOnMobile : true, // false will show top navigation bar on mobile devices
-  //       hideCloseButtonOnMobile : false, // true will hide the close button on mobile devices
-  //       //removeBarsOnMobile : true,  false will show bottom bar on mobile devices
-  //       hideBarsDelay : 20000000, // delay before hiding bars on desktop
-  //       videoMaxWidth : 1140,
-  //       loopAtEnd: false,
-  //       autoplayVideos: false,
-  //       useSVG: true
-  //     });
-  //   });
-  // });
 
 
 });
