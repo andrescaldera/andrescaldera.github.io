b975bbab89dfaa6b9acfa7e89a65f4ca8631306f
DavidCortizo <cortizo7269@gmail.com>
2017-03-20 15:14:22 +0100

    banner reworked
    added shadows

--- js/feed.js
+++ js/feed.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
       accessToken: '3457657437.de20c8b.743705c02eb048c69db553ef7b562af1',
       get: 'user',
       userId: '3457657437',
-      template: '<div><a href="{{link}}" target="_blank"><img src="{{image}}" width="230" height="164"/></a></div>',
+      template: '<div><a href="{{link}}" target="_blank" class="shadow-2"><img src="{{image}}" width="230" height="164"/></a></div>',
       sortBy: 'most-recent',
       limit: '1',
       resolution: 'standard_resolution'
@@ -19,7 +19,7 @@ $(document).ready(function(){
       accessToken: '3457657437.de20c8b.743705c02eb048c69db553ef7b562af1',
       get: 'user',
       userId: '3457657437',
-      template: '<div><a href="{{link}}" target="_blank"><img src="{{image}}" width="230" height="164"/></a></div>',
+      template: '<div><a href="{{link}}" target="_blank" class="shadow-2"><img src="{{image}}" width="230" height="164"/></a></div>',
       sortBy: 'most-recent',
       limit: '6',
       resolution: 'low_resolution'
