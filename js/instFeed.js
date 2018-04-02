869e0c5db577fc93c7858f6f2884b1498ccfef57
DavidCortizo <cortizo7269@gmail.com>
2018-04-02 18:01:40 +0200

    styles changed
    added new shadows and logo

--- js/instFeed.js
+++ js/instFeed.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
       accessToken: '3457657437.3ba8f29.28ecae7c9fa945208aa644d30406815e',
       get: 'user',
       userId: '3457657437',
-      template: '<div><a href="{{link}}" target="_blank" class="shadow-2"><img src="{{image}}" width="230" height="164"/></a></div>',
+      template: '<div><a href="{{link}}" target="_blank" class="shadow-1"><img src="{{image}}" width="230" height="164"/></a></div>',
       sortBy: 'most-recent',
       limit: '1',
       resolution: 'standard_resolution'
@@ -20,7 +20,7 @@ $(document).ready(function(){
       accessToken: '3457657437.3ba8f29.28ecae7c9fa945208aa644d30406815e',
       get: 'user',
       userId: '3457657437',
-      template: '<div><a href="{{link}}" target="_blank" class="shadow-2"><img src="{{image}}" width="230" height="164"/></a></div>',
+      template: '<div><a href="{{link}}" target="_blank" class="shadow-1"><img src="{{image}}" width="230" height="164"/></a></div>',
       sortBy: 'most-recent',
       limit: '6',
       resolution: 'low_resolution'
